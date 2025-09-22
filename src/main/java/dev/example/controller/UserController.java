c0734e0f6314d31b61571fc732d61250252246bb
microness <youjungho92@naver.com>
2025-09-22 21:50:00 +0900

    Update UserController.java

--- src/main/java/dev/example/controller/UserController.java
+++ src/main/java/dev/example/controller/UserController.java
@@ -1,5 +1,7 @@
 package dev.example.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -7,6 +9,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
+
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
 
     private final List<User> users = new ArrayList<>();
 
@@ -18,12 +22,14 @@ public class UserController {
 
     @GetMapping
     public List<User> getAll() {
+        log.info("GET /api/users 호출됨, 총 사용자 수: ", users.size());
         return users;
     }
 
     @PostMapping
     public User create(@RequestBody User user) {
         users.add(user);
+        log.info("New user created: {}", user.getName());
         return user;
     }
 
