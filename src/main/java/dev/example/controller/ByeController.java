76f2db22ece6c5cb359b8aac2be5db393cd69d61
Bellmin <whdals011203@naver.com>
2025-09-25 15:59:13 +0900

    feat: byeController 추가

---
+++ src/main/java/dev/example/controller/ByeController.java
@@ -0,0 +1,15 @@
+package dev.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/bye")
+@RestController
+public class ByeController {
+
+    @GetMapping
+    public String bye(){
+        return "bye";
+    }
+}
